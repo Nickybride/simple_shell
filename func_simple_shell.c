19b1c50cd5452fd2b90ecd330108f8563c7e1df8
sams-ddev <sams.alxstudent@gmail.com>
2023-08-23 23:30:53 +0000

    gcc 5.Simple_shell-1.0.c

--- func_simple_shell.c
+++ func_simple_shell.c
@@ -24,7 +24,6 @@ int opt_chars_read(void)
 		exit(1);
 	}
 }
-
 /**
  * handle - a function that handle NULL errors
  */
@@ -48,6 +47,7 @@ void handle_error(char *error_message)
 
 void remove_newline(char *cmd, ssize_t chars_read)
 {
+
 	/* Remove the newline character from the command */
 	if (cmd[chars_read - 1] == '\n')
 		cmd[chars_read - 1] = '\0';
@@ -86,7 +86,7 @@ char *get_path(char *cmd)
 	char *path = strtok(path_env, ":");
 
 	while (path != NULL)
-        {
+	{
 		char *full_path = malloc(strlen(path) + strlen(cmd) + 2);
 
 		if (full_path == NULL)
@@ -112,63 +112,11 @@ char *get_path(char *cmd)
  */
 void hanndle_env_cmd(void)
 {
-	char environ;
-
 	char **env = environ;
 	while (*env != NULL)
 	{
 		printf("%s\n", *env);
 		env++;
-		 continue;
 	}
-}
-
-/**
- * main - entry point to the program
- *
- * Return:  0.
- */
-
-int main(void)
-{
-	char *cmd = NULL;
-	size_t chars_read, buffsize = 0;
-
-	while (1)
-	{
-		printf("$ ");
-		chars_read = getline(&cmd, &buffsize, stdin);
-
-		if (chars_read == -1)
-			opt_chars_read();
-		remove_newline(char *cmd, ssize_t chars_read);
-		if (strcmp(cmd, "exit") == 0)	 /* Handle exit command */
-		{
-			free(cmd);
-			exit(EXIT_SUCCESS);
-		}
-		else if (strcmp(cmd, "env") == 0)	 /* Handle env command */
-			hanndle_env_cmd();
-		char *full_path = get_path(cmd);
-
-		if (full_path == NULL)
-		{
-			printf("Command not found: %s\n", cmd);
-			continue;
-		}
-		pid_t child_pid = fork();
-
-		if (child_pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		if (child_pid == 0) /* Child process */
-			handleKid_pid(const char *cmd);
-		else /* Parent process */
-			wait(NULL); /* Wait for the child process to finish */
-		free(full_path);
-	}
-	free(cmd);
-	return (0);
+	continue;
 }
